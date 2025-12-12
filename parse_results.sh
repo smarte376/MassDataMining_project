54c8386696ef896f78343b69e55b9f569e4cd7f1
jamesdupark <jamespark255@gmail.com>
2025-12-12 15:32:48 -0500

    feat: parse_all for running on all dirs in results

--- parse_results.sh
+++ parse_results.sh
@@ -7,8 +7,10 @@ fi
 
 old_wd=$(pwd)
 cd $2
+set -o noclobber
 grep -i "^classifying" *results.txt | 
 awk '{print $2,$4,$10}' |
 sed -n -e 's/\/\([0-9]*\)//' -e 's/_\S*//g' -e "s/$1/real/I" -e 's/ /,/gp' |
 tr '[:upper:]' '[:lower:]' > $3
 cd $old_wd
+set +o noclobber
