26b13080ba7f96297b138dac752ee2c0189727de
AmazingAndrewM1 <michaea3@tcnj.edu>
2025-12-15 16:26:34 -0500

    get_metrics.py is not capable of calculating the f1_score.

--- metrics/get_metrics.py
+++ metrics/get_metrics.py
@@ -1,41 +1,23 @@
 from pathlib import Path
 import pandas as pd
+from sklearn import metrics
 
 RESULTS_DIR = "../results/"
 
-def read_until_results_line(file):
-    target_line = "Loading network..."
-    for line in file:
-        if target_line in line:
-            return
-    raise ValueError("Cannot find line of results")
-
 def main():
     results_root = Path(RESULTS_DIR)
-    results_txt_path = next(results_root.rglob("classification_results/*.txt"), None)
-    if results_txt_path is None:
+    results_csv_path = next(results_root.rglob("results.csv"), None)
+
+    if results_csv_path is None:
         raise ValueError("No classification results found. Run run_classification_pipeline.py to generate results")
 
-    image_names = []
-    image_classifications = []
+    df = pd.read_csv(results_csv_path, header=None)
 
-    with open(Path(results_txt_path), "r") as file:
-        read_until_results_line(file)
-        for line in file:
-            line_split = line.split()
+    true_class = df.iloc[:, 1]
+    predicted_class = df.iloc[:, 2]
 
-            if line_split[0] != "Classifying":
-                break
-            
-            image_names.append(line_split[3][:-1]) # Remove the trailing colon
-            image_classifications.append(line_split[-1])
-
-    df = pd.DataFrame(data={
-        "name": image_names,
-        "classified_as": image_classifications
-    })
-
-    print(df.head())
+    f1_score = metrics.f1_score(true_class, predicted_class, average="macro")
+    print(f"f1_score: {f1_score}")
         
 if __name__ == "__main__":
     main()
